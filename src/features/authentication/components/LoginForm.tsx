dd2ba327d7a31439277003556f2cd46e8f335671
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-08-04 20:57:50 +0100

    login form completion

--- src/features/authentication/components/LoginForm.tsx
+++ src/features/authentication/components/LoginForm.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Form, Input } from "antd";
+import {
+  emailValidationRules,
+  generalValidationRules,
+} from "utils/formValidations";
 
 export const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
+    <Form layout="vertical" className="mt-5" requiredMark={false}>
+      <Form.Item name="email" label="Email" rules={emailValidationRules}>
+        <Input className="py-2" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={generalValidationRules}
+      >
+        <Input className="py-2" />
+      </Form.Item>
+
+      <div className="flex justify-center">
+        <button className="button w-56">Login</button>
+      </div>
+    </Form>
+  );
+};
--- src/features/authentication/pages/Login.tsx
+++ src/features/authentication/pages/Login.tsx
@@ -1,10 +1,6 @@
 import authBg from "../assets/images/authBg.png";
 import logo from "assets/images/logo.png";
-import { Form, Input } from "antd";
-import {
-  emailValidationRules,
-  generalValidationRules,
-} from "utils/formValidations";
+import { LoginForm } from "../components/LoginForm";
 
 const Login = () => {
   return (
@@ -16,7 +12,7 @@ const Login = () => {
         style={{ height: "70vh" }}
       />
       <div className="Container">
-        <div className="bg-white p-6 max-w-lg mx-auto shadow -mt-52 relative z-30">
+        <div className="bg-white p-7 max-w-md mx-auto shadow -mt-56 relative z-30">
           <div className="flex justify-between items-center">
             <div>
               <h3 className="text-lg font-semibold pb-1">Login</h3>
@@ -24,19 +20,7 @@ const Login = () => {
             </div>
             <img src={logo} alt="logo" className="h-1w-16 w-16 rounded-full" />
           </div>
-
-          <Form layout="vertical" className="mt-5" requiredMark={false}>
-            <Form.Item name="email" label="Email" rules={emailValidationRules}>
-              <Input />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              label="Password"
-              rules={generalValidationRules}
-            >
-              <Input />
-            </Form.Item>
-          </Form>
+          <LoginForm />
         </div>
       </div>
     </div>
