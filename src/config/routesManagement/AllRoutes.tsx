296b94af52168c02775e285c759706de93682ec0
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-08-05 12:18:21 +0100

    client page setup

--- src/config/routesManagement/AllRoutes.tsx
+++ src/config/routesManagement/AllRoutes.tsx
@@ -7,6 +7,8 @@ import {
 import { routePath } from "./routePath";
 import Login from "features/authentication/pages/Login";
 import Workspace from "features/workspace/Workspace";
+import Clients from "features/clients/pages/Clients";
+import ClientDetails from "features/clients/pages/ClientDetails";
 
 export const AllRoutes = () => {
   return (
@@ -14,6 +16,11 @@ export const AllRoutes = () => {
       <Routes>
         <Route path={routePath.login} element={<Login />} />
         <Route path={routePath.workspace} element={<Workspace />} />
+        <Route path={routePath.clients} element={<Clients />} />
+        <Route
+          path={routePath.clientDetails().format}
+          element={<ClientDetails />}
+        />
         <Route
           path={routePath.home}
           element={<Navigate to={routePath.login} />}
